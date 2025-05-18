d89a9291b2e3b49a02be09f9e5a7a76c59a19fe7
AmirHosein-Vahed <vahed.ahv@gmail.com>
2025-05-18 10:27:59 +0330

    Table model validator and Reservation model was created

--- booking/models.py
+++ booking/models.py
@@ -7,9 +7,25 @@ class Table(models.Model):
     Represents a table in the restaurant.
     """
     table_number = models.IntegerField(unique=True)
-    seats = models.IntegerField()
+    seats = models.IntegerField(validators=[
+        models.validators.MinValueValidator(4),
+        models.validators.MaxValueValidator(10)
+    ])
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return f"Table {self.table_number} ({self.seats} seats)"
     
+class Reservation(models.Model):
+    """
+    Represents a reservation made by a user.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    seats_booked = models.IntegerField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    booking_time = models.DateTimeField(auto_now_add=True)
+    is_cancelled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Reservation for {self.user.username} at Table {self.table.table_number}"
