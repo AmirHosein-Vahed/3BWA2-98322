9c908abb8da76188e240fe899f8e0e91d76abee2
AmirHosein-Vahed <vahed.ahv@gmail.com>
2025-05-18 10:40:56 +0330

    added constraint on number_of_indivisuals >= 1

--- booking/serializers.py
+++ booking/serializers.py
@@ -8,6 +8,8 @@ class ReservationSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'cost', 'booking_time', 'is_cancelled']
 
 class BookingSerializer(serializers.ModelSerializer):
+    number_of_individuals = serializers.IntegerField(min_value=1)
+
     class Meta:
         model = Table
         fields = '__all__'
