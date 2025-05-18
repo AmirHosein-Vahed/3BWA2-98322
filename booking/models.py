8162316b491f56744a1bbae8048c5f7e96e11466
AmirHosein-Vahed <vahed.ahv@gmail.com>
2025-05-18 10:22:11 +0330

    Table model was created

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Table(models.Model):
+    """
+    Represents a table in the restaurant.
+    """
+    table_number = models.IntegerField(unique=True)
+    seats = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Table {self.table_number} ({self.seats} seats)"
+    
